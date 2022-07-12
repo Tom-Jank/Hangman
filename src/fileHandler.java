ac7808c17aed6243e0c6f18d73cbb75fc67cd751
Tom-Jank <tjank@deloitte.com>
2022-07-12 16:53:55 +0200

    Add method that writed buzzword to text DB

--- src/fileHandler.java
+++ src/fileHandler.java
@@ -1,6 +1,4 @@
-import java.io.FileOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 public class fileHandler {
     protected static void createHangmanWordDatabase() {
@@ -13,5 +11,19 @@ public class fileHandler {
         catch(Exception e) {
             e.printStackTrace();
         }
+    }
+    protected static void writeBuzzwordToDatabase() {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter("DATABASE.txt", true));
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter buzzword: ");
+            String buzzword = scanner.nextLine().trim().toUpperCase();
+            writer.append(buzzword + "\n");
+            writer.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
