47e8c91b7913df4886ae78397442fe87bda77822
Tom-Jank <tjank@deloitte.com>
2022-07-13 17:05:57 +0200

    Fixed: Displaying user progress

--- src/gameLogic.java
+++ src/gameLogic.java
@@ -10,10 +10,18 @@ public class gameLogic {
         else return true;
     }
     protected static String updatePlayerProgress(String buzzword, char userInput, String mappedBuzzword) {
-        for (int i = 0; i <= buzzword.length(); i++) {
-            if (buzzword.charAt(i) == userInput)
-                mappedBuzzword = mappedBuzzword.substring(0, i) + userInput + mappedBuzzword.substring(i + 1);
-            return mappedBuzzword;
+        //for (int i = 0; i <= buzzword.length(); i++) {
+        //    if (buzzword.charAt(i) == userInput)
+        //        mappedBuzzword = mappedBuzzword.substring(0, i) + userInput + mappedBuzzword.substring(i + 1);
+         //   return mappedBuzzword;
+        //}
+
+        if (buzzword.indexOf(userInput) != -1) {
+            int index = buzzword.indexOf(userInput);
+            while (index != -1) {
+                mappedBuzzword = mappedBuzzword.substring(0, index) + userInput + mappedBuzzword.substring(index + 1);
+                index = buzzword.indexOf(userInput, index + 1);
+            }
         }
         return mappedBuzzword;
     }
