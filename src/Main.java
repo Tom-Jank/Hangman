bcd0594442c9117402490917043e8a3d7579ebd8
Tom-Jank <tjank@deloitte.com>
2022-07-13 16:38:08 +0200

    Added: - Informing player of game result - Session handler - Fixed the problem of program running constantly

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,8 @@ public class Main {
             char decision = TUI.userMainUI();
             switch(decision) {
                 case '1':
-                    gameLogic player = new gameLogic();
-                    String buzzword = fileHandler.selectRandomBuzzword();
-                    String mappedBuzzword = TUI.returnMappedBuzzword(buzzword);
-                    while(true) {
-                        System.out.println(mappedBuzzword);
-                        char answer = TUI.gameMenu();
-                        boolean isUserCorrect = gameLogic.checkIfPlayerIsCorrect(buzzword,answer);
-                        if(!isUserCorrect) player.playerLifeCounter -= 1;
-                        else {
-                            mappedBuzzword = gameLogic.updatePlayerProgress(buzzword,answer,mappedBuzzword);
-                        }
-                        if(!player.isStillPlaying) break;
-                    }
+                    Session.initializeAndManageSession();
+                    break;
                 case '2':
                     fileHandler.writeBuzzwordToDatabase();
                     break;
