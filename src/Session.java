a8c9ca69af2af10789fcc33f3444fd29377e1126
Tom-Jank <tjank@deloitte.com>
2022-07-13 16:44:36 +0200

    Fixed: Added tracking for session handler

---
+++ src/Session.java
@@ -0,0 +1,27 @@
+public class Session {
+    protected static void initializeAndManageSession() {
+        gameLogic player = new gameLogic();
+        String buzzword = fileHandler.selectRandomBuzzword();
+        String mappedBuzzword = TUI.returnMappedBuzzword(buzzword);
+        while(true) {
+            System.out.println(mappedBuzzword);
+            char answer = TUI.gameMenu();
+            boolean isUserCorrect = gameLogic.checkIfPlayerIsCorrect(buzzword,answer);
+            if(!isUserCorrect){
+                player.playerLifeCounter -= 1;
+                if(player.playerLifeCounter == 0) player.isStillPlaying = false;
+            }
+            else {
+                mappedBuzzword = gameLogic.updatePlayerProgress(buzzword,answer,mappedBuzzword);
+            }
+            if(!player.isStillPlaying){
+                break;
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
